1f0a96e62ee184707d843b8d3a6d5ff88bfde651
Charles <charles.lana@expertcode.com.br>
2022-12-28 15:00:28 -0300

    update

--- client/assets/js/client.js
+++ client/assets/js/client.js
@@ -2,13 +2,35 @@ import { io } from '../lib/socket.io/socket.io.esm.min.js';
 
 const socket = io('http://localhost:3000');
 
+const userSocket = io('http://localhost:3000/user', {
+  auth: {
+    token: 'Test',
+  },
+});
+
+userSocket.on('connect_error', error => {
+  displayMessage(error);
+});
+
 socket.on('connect', () => {
   displayMessage(`You connected with id: ${socket.id}`);
+});
+
+socket.on('disconnect', () => {
+  displayMessage(`You disconnected`);
 });
 
 // socket.emit('custom-event', 10, 'Hi', { a: 'a' });
 
 socket.on('receive-message', message => {
+  displayMessage(message);
+});
+
+socket.on('user-connected', message => {
+  displayMessage(message);
+});
+
+socket.on('user-disconnected', message => {
   displayMessage(message);
 });
 
@@ -25,12 +47,15 @@ form.addEventListener('submit', e => {
     return;
   }
   displayMessage(message);
-  socket.emit('send-message', message);
+  socket.emit('send-message', message, room);
   messageInput.value = '';
 });
 
 joinRoomButton.addEventListener('click', () => {
   const room = roomInput.value;
+  socket.emit('join-room', room, message => {
+    displayMessage(message);
+  });
 });
 
 function displayMessage(message) {
@@ -38,3 +63,23 @@ function displayMessage(message) {
   div.textContent = message;
   document.getElementById('message-container').append(div);
 }
+
+// let count = 0;
+
+// setInterval(() => {
+//   socket.volatile.emit('ping', ++count);
+// }, 1000);
+
+document.addEventListener('keydown', e => {
+  if (e.target.matches('input')) {
+    return;
+  }
+
+  if (e.key === 'c') {
+    socket.connect();
+  }
+
+  if (e.key === 'd') {
+    socket.disconnect();
+  }
+});
--- server/src/server.js
+++ server/src/server.js
@@ -1,18 +1,89 @@
-const io = require('socket.io')(3000, {
+const cors = require('cors');
+const { instrument } = require('@socket.io/admin-ui');
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: ['http://127.0.0.1:5500', 'https://admin.socket.io'],
+    credentials: true,
   },
 });
 
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (_req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+const userIo = io.of('/user');
+
+userIo.on('connection', socket => {
+  console.log(`connected to user namespace with username ${socket.username}`);
+
+  socket.on('disconnect', reason => {
+    console.log(
+      `disconnected to user namespace with username ${socket.username}`
+    );
+  });
+});
+
+userIo.use((socket, next) => {
+  if (socket.handshake.auth.token) {
+    socket.username = getUsernameFromToken(socket.handshake.auth.token);
+    next();
+    return;
+  }
+  next(new Error('Please send token'));
+});
+
+function getUsernameFromToken(token) {
+  return token;
+}
+
 io.on('connection', socket => {
-  console.log(socket.id);
+  console.log(`user ${socket.id} connected`);
+  socket.broadcast.emit('user-connected', `user ${socket.id} connected`);
 
   // socket.on('custom-event', (number, string, obj) => {
   //   console.log(number, string, obj);
   // });
 
-  socket.on('send-message', message => {
-    socket.broadcast.emit('receive-message', message);
-    // console.log(message);
+  socket.on('send-message', (message, room) => {
+    if (room === '') {
+      socket.broadcast.emit('receive-message', `${socket.id}: ${message}`);
+      // console.log(message);
+      return;
+    }
+    socket.to(room).emit('receive-message', `${socket.id}: ${message}`);
   });
+
+  socket.on('join-room', (room, callback) => {
+    socket.join(room);
+    callback(`Joined ${room}`);
+  });
+
+  socket.on('ping', count => {
+    console.log(count);
+  });
+
+  socket.on('disconnect', reason => {
+    console.log(`user ${socket.id} disconnected, reason ${reason}`);
+    socket.broadcast.emit(
+      'user-disconnected',
+      `user ${socket.id} disconnected`
+    );
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+instrument(io, {
+  auth: false,
 });
